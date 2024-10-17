90d2b395a4f6ab62de395197f4c6f6a30a3d888d
Filip Hanout <hanoutfilip@gmail.com>
2024-10-17 17:10:56 +0200

    Mapa s pointama

--- khk2024/src/components/HomeComponent.jsx
+++ khk2024/src/components/HomeComponent.jsx
@@ -1,100 +1,105 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from "react";
 import "../App.css";
 
 const HomeComponent = () => {
-    const [geoData, setGeoData] = useState(null); // Stav pro GeoJSON data
-    const [filteredData, setFilteredData] = useState([]); // Stav pro filtrovaná data
-    const [searchQuery, setSearchQuery] = useState(''); // Stav pro vyhledávací query
-    const [dropdownOpen, setDropdownOpen] = useState(false); // Stav pro dropdown otevření/zavření
-    const dropdownRef = useRef(null); // Reference pro dropdown div
+  const [geoData, setGeoData] = useState(null); // Stav pro GeoJSON data
+  const [filteredData, setFilteredData] = useState([]); // Stav pro filtrovaná data
+  const [searchQuery, setSearchQuery] = useState(""); // Stav pro vyhledávací query
+  const [dropdownOpen, setDropdownOpen] = useState(false); // Stav pro dropdown otevření/zavření
+  const dropdownRef = useRef(null); // Reference pro dropdown div
 
-    useEffect(() => {
-        // Fetch GeoJSON data z backendu
-        fetch("http://localhost:8000/message") // URL backendu
-            .then((res) => {
-                if (!res.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return res.json();
-            })
-            .then((data) => {
-                setGeoData(data); // Uložení dat do stavu
-                setFilteredData(data.features); // Nastavení počátečního zobrazení všech dat
-            })
-            .catch((error) => {
-                console.error("Error fetching the GeoJSON:", error);
-            });
-    }, []);
-
-    useEffect(() => {
-        // Funkce pro skrytí dropdownu při kliknutí mimo
-        const handleClickOutside = (event) => {
-            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-                setDropdownOpen(false);
-            }
-        };
-
-        // Přidání posluchače události
-        document.addEventListener('mousedown', handleClickOutside);
-
-        // Cleanup function, která se zavolá při unmountování komponenty
-        return () => {
-            document.removeEventListener('mousedown', handleClickOutside);
-        };
-    }, []);
-
-    // Funkce pro filtrování dat na základě vyhledávání
-    const handleSearch = (e) => {
-        const query = e.target.value.toLowerCase();
-        setSearchQuery(query);
-        if (geoData) {
-            const filtered = geoData.features.filter(feature =>
-                feature.properties.nazev.toLowerCase().includes(query)
-            );
-            setFilteredData(filtered);
+  useEffect(() => {
+    // Fetch GeoJSON data z backendu
+    fetch("http://localhost:8000/kultura") // URL backendu
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Network response was not ok");
         }
+        return res.json();
+      })
+      .then((data) => {
+        setGeoData(data); // Uložení dat do stavu
+        setFilteredData(data.features); // Nastavení počátečního zobrazení všech dat
+      })
+      .catch((error) => {
+        console.error("Error fetching the GeoJSON:", error);
+      });
+  }, []);
+
+  useEffect(() => {
+    // Funkce pro skrytí dropdownu při kliknutí mimo
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setDropdownOpen(false);
+      }
     };
 
-    // Funkce pro výběr položky z dropdownu
-    const handleOptionClick = (nazev) => {
-        console.log('Selected option:', nazev);
-        setSearchQuery(nazev);
-        setDropdownOpen(false); // Zavřeme dropdown po výběru
+    // Přidání posluchače události
+    document.addEventListener("mousedown", handleClickOutside);
+
+    // Cleanup function, která se zavolá při unmountování komponenty
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
     };
+  }, []);
 
+  // Funkce pro filtrování dat na základě vyhledávání
+  const handleSearch = (e) => {
+    const query = e.target.value.toLowerCase();
+    setSearchQuery(query);
+    if (geoData) {
+      const filtered = geoData.features.filter((feature) =>
+        feature.properties.nazev.toLowerCase().includes(query)
+      );
+      setFilteredData(filtered);
+    }
+  };
 
-    return (
-        <div className="geo-container">
-            <h2>GeoJSON Data:</h2>
-            <div className="dropdown" ref={dropdownRef}>
-                <input
-                    type="text"
-                    value={searchQuery}
-                    onChange={handleSearch}
-                    placeholder="Vyhledat..."
-                    className="dropdown-btn"
-                    onClick={() => setDropdownOpen(!dropdownOpen)} // Toggle otevření dropdownu
-                />
-                {dropdownOpen && (
-                    <div className="dropdown-content">
-                        {filteredData.length > 0 ? (
-                            filteredData.map((feature, index) => (
-                                <a
-                                    href="#"
-                                    key={index}
-                                    onClick={() => handleOptionClick(feature.properties.nazev)}
-                                >
-                                    {feature.properties.nazev}
-                                </a>
-                            ))
-                        ) : (
-                            <p>No results found</p>
-                        )}
-                    </div>
-                )}
+  // Funkce pro výběr položky z dropdownu
+  const handleOptionClick = (nazev) => {
+    console.log("Selected option:", nazev);
+    setSearchQuery(nazev);
+    setDropdownOpen(false); // Zavřeme dropdown po výběru
+  };
+
+  return (
+    <>
+      <div className="geo-container">
+        <h2>GeoJSON Data:</h2>
+        <div className="dropdown" ref={dropdownRef}>
+          <input
+            type="text"
+            value={searchQuery}
+            onChange={handleSearch}
+            placeholder="Vyhledat..."
+            className="dropdown-btn"
+            onClick={() => setDropdownOpen(!dropdownOpen)} // Toggle otevření dropdownu
+          />
+          {dropdownOpen && (
+            <div className="dropdown-content">
+              {filteredData.length > 0 ? (
+                filteredData.map((feature, index) => (
+                  <a
+                    href="#"
+                    key={index}
+                    onClick={() => handleOptionClick(feature.properties.nazev)}
+                  >
+                    {feature.properties.nazev}
+                  </a>
+                ))
+              ) : (
+                <p>No results found</p>
+              )}
             </div>
+          )}
         </div>
-    );
+        {/* <div>
+            <h1>{feature.properties.nazev}</h1>
+            <p>{feature.properties.popis}</p>
+        </div> */}
+      </div>
+    </>
+  );
 };
 
 export default HomeComponent;
--- khk2024/src/components/MapComponent.jsx
+++ khk2024/src/components/MapComponent.jsx
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from "react";
-import L from "leaflet";
+import L, { Draggable, setOptions } from "leaflet";
 import "leaflet/dist/leaflet.css";
+import blicon from "../assets/blackPoint.svg";
+import reicon from "../assets/redPoint.svg";
 
 const MapComponent = () => {
-  const [geoData, setGeoData] = useState(null); // State to hold GeoJSON data
+  const [geoData, setGeoData] = useState(null); // State to hold GeoJSON data for cultural points
+  const [autoData, setAutoData] = useState(null); // State to hold GeoJSON data for bus stops
+  const [selectedPoints, setSelectedPoints] = useState([]); // Store clicked points
+  const [map, setMap] = useState(null); // Store Leaflet map instance
 
   useEffect(() => {
-    // Fetch GeoJSON data
-    fetch("http://localhost:8000/kultura") // Your API endpoint
+    // Fetch GeoJSON data for cultural points
+    fetch("http://localhost:8000/kultura") // Your API endpoint for cultural points
       .then((res) => {
         if (!res.ok) {
           throw new Error("Network response was not ok");
@@ -20,12 +25,27 @@ const MapComponent = () => {
       .catch((error) => {
         console.error("Error fetching the GeoJSON data:", error);
       });
+
+    // Fetch GeoJSON data for bus stops
+    fetch("http://localhost:8000/autobusy") // Your API endpoint for bus stops
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setAutoData(data); // Set the fetched GeoJSON data to state
+      })
+      .catch((error) => {
+        console.error("Error fetching the bus stops GeoJSON data:", error);
+      });
   }, []);
 
   useEffect(() => {
-    if (geoData) {
-      // Initialize the map when geoData is available
-      const map = L.map("map").setView(
+    if (geoData && autoData) {
+      // Initialize the map when geoData and autoData are available
+      const newMap = L.map("map").setView(
         [
           geoData.features[0].geometry.coordinates[1], // Latitude
           geoData.features[0].geometry.coordinates[0], // Longitude
@@ -33,24 +53,95 @@ const MapComponent = () => {
         13
       );
 
+      let customIcon = {
+        iconUrl:blicon,
+        iconSize:[40,40]
+      };    
+      let myIcon = L.icon(customIcon);
+      let iconOptions = {
+        title:"{autoData.feature.properties.nazev}",
+        Draggable:false,
+        icon:myIcon
+      };
+
       // Set up the OSM tile layer
       L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
+      }).addTo(newMap);
+
+      // Store the map instance
+      setMap(newMap);
 
       // Add a marker for each feature in geoData
       geoData.features.forEach((feature) => {
         const [longitude, latitude] = feature.geometry.coordinates;
-
-        L.marker([latitude, longitude])
-          .addTo(map)
+        // Add marker with click event
+        const marker = L.marker([latitude, longitude], iconOptions)
+          .addTo(newMap)
           .bindPopup(`<b>${feature.properties.nazev}</b>`); // Display the name in a popup
+        // On marker click, handle selection
+        marker.on("click", () => {
+          if (selectedPoints.length < 2) {
+            setSelectedPoints((prev) => [...prev, { latitude, longitude }]);
+          }
+        });
+      });
+
+      // Add a marker for each feature in autoData
+      autoData.features.forEach((feature) => {
+        const [longitude, latitude] = feature.geometry.coordinates;
+        // Add marker with click event
+        const marker = L.marker([latitude, longitude])
+          .addTo(newMap)
+          .bindPopup(`<b>${feature.properties.nazev}</b>`); // Display the name in a popup
+        // On marker click, handle selection
+        marker.on("click", () => {
+          if (selectedPoints.length < 2) {
+            setSelectedPoints((prev) => [...prev, { latitude, longitude }]);
+          }
+        });
       });
     }
-  }, [geoData]); // Run this effect when geoData changes
+  }, [geoData, autoData]);
 
-  return <div id="map" style={{ height: "500px", width: "100%", padding: "5rem"}}></div>;
+  // Calculate walking route when two points are selected
+  useEffect(() => {
+    if (selectedPoints.length === 2 && map) {
+      const [point1, point2] = selectedPoints;
+      // Fetch the route from ORS API for walking
+      const apiKey = "5b3ce3597851110001cf6248c7f3ebda7a7c4bf6bfd4ec13a4df091e"; // Replace with your ORS API key
+      const url = `https://api.openrouteservice.org/v2/directions/foot-walking?api_key=${apiKey}&start=${point1.longitude},${point1.latitude}&end=${point2.longitude},${point2.latitude}`;
+      fetch(url)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          // Draw the route on the map
+          const route = data.features[0].geometry.coordinates;
+          const latlngs = route.map((coord) => [coord[1], coord[0]]);
+          L.polyline(latlngs, { color: "blue" }).addTo(map); // Add route as a polyline to the map
+          map.fitBounds(latlngs); // Adjust the map view to fit the route
+        })
+        .catch((error) => {
+          console.error("Error fetching route:", error);
+        });
+      // Clear selected points after fetching the route
+      setSelectedPoints([]);
+    }
+  }, [selectedPoints, map]); // Run this effect when selectedPoints or map changes
+
+  return (
+    <>
+      <div
+        id="map"
+        style={{ height: "500px", width: "100%", padding: "5rem" }}
+      ></div>
+    </>
+  );
 };
 
 export default MapComponent;
