505dddfbb76559d241a7c4ff2e4fb1c318084173
David96cz <963dasty@seznam.cz>
2024-10-17 13:35:33 +0200

    asfafsd

--- khk2024/src/components/HeaderComponent.jsx
+++ khk2024/src/components/HeaderComponent.jsx
@@ -1,14 +1,31 @@
 import { useState, useEffect } from "react";
 import "../App.css";
+import logo from "../assets/logobus.svg";
 
 function Header() {
 
   return (
-    <div className="App">
-      <h1>
-        HEADER
-      </h1>
-    </div>
+    <header>
+        <div class="logo">
+            <img src={logo} alt="Logo webu"></img>
+        </div>
+
+        <div class="menu-toggle" id="menu-toggle">
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+        </div>
+
+        <nav id="nav">
+            <ul>
+                <li><a href="stranka1.html">Stránka 1</a></li>
+                <li><a href="stranka2.html">Stránka 2</a></li>
+                <li><a href="stranka3.html">Stránka 3</a></li>
+                <li><a href="stranka4.html">Stránka 4</a></li>
+                <li><a href="stranka5.html">Stránka 5</a></li>
+            </ul>
+        </nav>
+    </header>
   );
 }
 
